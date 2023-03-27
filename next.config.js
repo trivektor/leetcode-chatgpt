2eefc50e2dee5b8f21aa0034fd8a16d05a7fffae
Tri Vuong <trivektor@gmail.com>
2023-03-27 10:31:09 +0700

    redirect / to /questions

--- next.config.js
+++ next.config.js
@@ -15,6 +15,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/questions",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
